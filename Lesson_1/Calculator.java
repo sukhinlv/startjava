1f704bf0ba93cc52d7e0913e3c8e62315a8d9f42
sukhinlv <107252295+sukhinlv@users.noreply.github.com>
2022-06-14 11:48:07 +0300

    Create Calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        char sign = '^';
+
+        System.out.println("Калькулятор");
+        int result = 0;
+        if (sign == '+') {
+            result = a + b;
+        } else if (sign == '-') {
+            result = a - b;
+        } else if (sign == '*') {
+            result = a * b;
+        } else if (sign == '/') {
+            result = a / b;
+        } else if (sign == '^') {
+            result = a;
+            for (int i = 2; i <= b; i++) {
+                result *=  a;
+            }
+        } else if (sign == '%') {
+            result = a % b;
+        } else {
+            System.out.println("Неизвестное вычисление!");
+        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+}
