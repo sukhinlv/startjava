77c42ad786994112375959bfe6134177ad3cd28f
sukhinlv <107252295+sukhinlv@users.noreply.github.com>
2022-06-15 21:49:11 +0300

    правки после проверки

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -5,21 +5,21 @@ public class MyFirstGame {
         // число, с которого стартует игрок
         int playerNumber = 33;
         // границы загаданного числа
-        int minBorder = 1;
-        int maxBorder = 100;
+        int startRange = 1;
+        int endRange = 100;
         while (true) {
             if (playerNumber == randomNumber) {
-                System.out.println("Вы победили!");
                 break;
             } else if (playerNumber > randomNumber) {
                 System.out.format("Число %d больше того, что загадал компьютер%n", playerNumber);
-                maxBorder = playerNumber - 1;
+                endRange = playerNumber - 1;
             } else if (playerNumber < randomNumber) {
                 System.out.format("Число %d меньше того, что загадал компьютер%n", playerNumber);
-                minBorder = playerNumber + 1;
+                startRange = playerNumber + 1;
             }
             // типа игрок выбирает новое число
-            playerNumber = minBorder + (maxBorder - minBorder) / (playerNumber % 3 + 1); 
+            playerNumber = startRange + (endRange - startRange) / (playerNumber % 3 + 1); 
         }
+        System.out.println("Вы победили!");
     }
 }
